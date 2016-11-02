0e62b4eb6c67ac5058de3140b937ac6b51ecf57a
041616 <041616@gmail.com>
2016-11-02 11:21:48 +0200

    demo5.js: 'indent' was added

--- example/js/demo05.js
+++ example/js/demo05.js
@@ -2,5 +2,5 @@ import stickyBlock from '../../src/stickyBlock';
 
 stickyBlock(
     document.getElementById('float-block'),
-    { relative: 'main', top: 20, bottom: 20 }
+    { relative: 'main', top: 20, bottom: 20, indent: 20 }
 );
